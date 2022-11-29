c3c03c760e9e0cdb0604369fec2c673067cf236f
Irina Tsarevski <irinatsarevski@icloud.com>
2022-11-29 09:59:46 +0100

    add C# Seninar 01

---
+++ C#Seminar01/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+//Домашняя работа 1.
+
+//Задача 1. Напишите программу, которая на вход принимает два числа  и проверяет, является ли первое число квадратом второго
+
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad = num2 * num2;
+if(num1 == quad)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+*/
+/*
+//Задача 2.
+//Написать программу, которая на вход принимает одно число (N), а на выходе  показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Input an integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current ++;
+}
+*/
+
+//Задача 3.
+
+/*
+12345 / 10 = 1234
+12345 / 100 = 123
+12345 / 1000 = 12
+12345 / 10000 = 1 
+
+с помощью целоцисленного деления мы отсекаем разряды
+
+12345 % 10 = 5
+12345 % 100 = 45
+12345 % 1000 = 345
+12345 % 10000 = 2345
+12345 % 100000 = 12345
+
+с помощью остатка от деления мы отсекаем старшие разряды
+
+чтобы из 191736 получить 917, надо %1000 и /100 
+*/
