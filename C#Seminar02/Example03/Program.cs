e7bcf03bc64dcf0fcac4b609443b18ac93b8e228
Irina Tsarevski <irinatsarevski@icloud.com>
2022-12-04 17:30:49 +0100

    Example 03, 04

--- C#Seminar02/Example03/Program.cs
+++ C#Seminar02/Example03/Program.cs
@@ -1,2 +1,25 @@
-﻿
-// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно a и b.
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно a и b.
+/*
+void Kratnost(int Nummer, int a, int b)
+{
+    
+    if(Nummer % a == 0 && Nummer % b == 0)
+    {
+        Console.WriteLine($"Число {Nummer} кратно {a} и {b}");
+    }
+    else
+    {
+        Console.WriteLine($"Число {Nummer} не кратно одновременно {a} и {b}");
+    }
+    
+}
+
+Console.WriteLine("Введите первое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Kratnost(num, A, B);
+*/
--- C#Seminar02/Example04/Program.cs
+++ C#Seminar02/Example04/Program.cs
@@ -1,1 +1,27 @@
 ﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+/*
+void Quadrat ( int num1, int num2 )
+{
+    if ( num1 * num1 == num2 )
+    {
+        Console.WriteLine ( $"число {num2} является квадратом числа {num1}" );
+    }
+    if ( num2 * num2 == num1 )
+    {
+        Console.WriteLine ( $"Число {num1} является квадратом числа {num2}" );
+    }
+    else
+    {
+        Console.WriteLine ( $"Ни одно из чисел не является квадратом другого числа" );
+    }
+    
+}
+
+Console.WriteLine("Введите первое число: ");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+
+Quadrat(Num1, Num2);
+*/
+
