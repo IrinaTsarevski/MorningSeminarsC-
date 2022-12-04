fd22adfd2a111e9fcc6ca02ab331887705cdc9d5
Irina Tsarevski <irinatsarevski@icloud.com>
2022-12-04 15:52:48 +0100

    Example 02

---
+++ C#Seminar02/Example01/Program.cs
@@ -0,0 +1,14 @@
+﻿int CutNumber(int num)
+{
+    int ed = num % 10;
+    int sot = num / 100;
+
+    int rusult = sot * 10 + ed;
+    return rusult;
+}
+
+
+int randNum = new Random().Next(100, 1000);
+int newNum = CutNumber(randNum);
+
+Console.WriteLine($"New version of a number {randNum} is {newNum}");
---
+++ C#Seminar02/Example02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int MaxNum( int num )
+{
+    des = num / 10;
+    ed = num % 10;
+    maxnummer = des;
+    if (maxnummer < ed)
+    {
+        maxnummer = ed;
+    }
+   return maxnummer
+}
+
+number = new Random().Next(10,100);
+Console.WriteLine(number);
+
+int maxnum = MaxNum(number);
+Console.WriteLine($"наибольшая цифра числа {number} : {maxnum}");
