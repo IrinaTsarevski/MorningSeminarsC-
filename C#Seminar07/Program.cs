541478766696551e2e989d36bdffdc3dcfa2f9d2
Irina Tsarevski <irinatsarevski@icloud.com>
2022-12-30 16:37:44 +0100

    added C#Seminar09

---
+++ C#Seminar07/Program.cs
@@ -0,0 +1,206 @@
+﻿/*int[,] CreateRandom2dArray()
+{
+    Console.WriteLine("Input a numbers of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input a numbers of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(minValue, maxValue +1);
+        }
+    }
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        Console.WriteLine();
+    }  
+    Console.WriteLine();
+}
+
+
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+*/
+
+//1. Задайте двумерный массив размера m на n, 
+//каждый элемент в массиве находится по формуле: 
+//Aij = i+j. Выведите полученный массив на экран.
+
+/*int[,] CreateRandom2dArray()
+{
+    Console.WriteLine("Input a numbers of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input a numbers of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i,j] = i + j;
+        }
+    }
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        Console.WriteLine();
+    }  
+    Console.WriteLine();
+}
+
+
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+*/
+
+//2. Задайте двумерный массив. Найдите элементы, 
+//у которых оба индекса чётные, и замените эти элементы 
+//на их квадраты
+
+/*int [,] CreateRandom2dArray()
+{
+    Console.WriteLine("Input a numbers of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input a numbers of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        Console.WriteLine();
+    }  
+    Console.WriteLine();
+}
+
+void Quadr(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i+=2)
+    {
+        for(int j = 0; j < array.GetLength(1); j+=2)
+        {
+            array[i,j] *= array[i,j];
+        }
+    }
+}
+
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+Quadr(myArray);
+Show2dArray(myArray);
+*/
+
+
+//3. Задайте двумерный массив. Найдите сумму элементов, 
+//находящихся на главной диагонали 
+//(с индексами (0,0); (1;1) и т.д.
+
+int[,] CreateRandom2dArray()
+{
+    Console.WriteLine("Input a numbers of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input a numbers of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(minValue, maxValue +1);
+        }
+    }
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        Console.WriteLine();
+    }  
+    Console.WriteLine();
+}
+
+/*
+int SumNum(int[,] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++) 
+        {
+            if(i == j)               //в этом варианте мы проверяем КАЖДЫЙ элемент, те в каждой строке проверим каждый столбик
+            sum = sum + array[i,j];
+        }
+    }
+    return sum;
+}
+*/
+int SumNum(int[,] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.GetLength(1) && i < array.GetLength(0); i++) // i прыгает не только по строкам, но и сразу по столбцам,
+            sum += array[i,i];                                            // те переходит сразу  на следующую строку и элемент
+}                                                                         // и половина элементов останется не проверенными
+    return sum;
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+int sum = SumNum(myArray);
+Console.WriteLine(sum);
---
+++ C#Seminar09/Program.cs
@@ -0,0 +1,69 @@
+﻿//Задайте значение N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от 1 до N.
+
+/*void ShowNums(int num)
+{
+    Console.Write(num + " ");
+    if(num > 1) ShowNums(num - 1);
+    Console.Write(num + " ");
+}
+ShowNums(5);
+*/
+
+
+//Напишите программу, 
+//которая будет принимать на вход число и возвращать сумму его цифр.
+
+/*int GetSumOfDigits(int num)
+{
+    if(num != 0) return GetSumOfDigits(num / 10) + num % 10;
+    else return 0;
+}
+Console.Write(GetSumOfDigits(417));
+*/
+
+
+//Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N.
+
+/*void ShowNaturNumbers(int numA, int numB)
+{
+    if(numA < numB) 
+    {
+        ShowNaturNumbers(numA, numB - 1);
+        Console.Write(numB + " ");
+    }
+    if(numB < numA) 
+    {
+        Console.Write(numA + " ");
+        ShowNaturNumbers(numA - 1, numB); 
+    }
+    if(numA == numB)
+        Console.Write(numA + " ");
+} 
+
+Console.Write("Input a first number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+ShowNaturNumbers(numberA, numberB);
+*/
+
+
+//Напишите программу, которая на вход принимает
+// два числа A и B, и возводит число А в целую степень B.
+
+/*double Power(int basic, int degree)
+{
+    if(degree > 0) return Power(basic, degree - 1) * basic;
+    else if(degree < 0) return Power(basic, degree + 1) / basic;
+    else return 1;
+}
+
+Console.Write("Input a first number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Power(numberA, numberB));
+*/
